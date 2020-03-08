a6397bedbeb38afa9c218f112614927191346bcc
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-08 15:51:47 -0700

    Create a Stack class

---
+++ 1.3 Stacks/Stack.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class Stack
+    {
+        private int[] items = new int[5];
+        private int count;
+
+        public void Push(int item)
+        {
+            if (count == items.Length)
+                throw new InvalidOperationException();
+
+            items[count++] = item;
+        } // Push method
+
+        public int Pop()
+        {
+            if (count == items.Length)
+                throw new InvalidOperationException();
+
+            return items[--count];
+        } // Pop method
+
+        public int Peek()
+        {
+            if (count == 0)
+                throw new InvalidOperationException();
+
+            return items[count - 1];
+        } // Peek method
+
+        public bool IsEmpty()
+        {
+            return count == 0;
+        } // IsEmpty method
+
+        public override string ToString()
+        {
+            var content = new int[count];
+            Array.Copy(items, content, count);
+            return $"[{string.Join(", ", content)}]";
+        } // ToString method
+
+
+
+
+    } // Stack class
+
+} // Stacks namespace
