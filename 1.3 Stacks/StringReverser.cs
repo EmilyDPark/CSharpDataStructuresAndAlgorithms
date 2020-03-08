1d1891da7dbbc5863023d4440ce9d8e4bd16e0d6
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-08 15:52:11 -0700

    Reverse a String Exercise

---
+++ 1.3 Stacks/StringReverser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacks
+{
+    class StringReverser
+    {
+        public string Reverse(string input)
+        {
+            if (input == null)
+                throw new InvalidOperationException();
+
+            var stack = new Stack<char>();
+
+            foreach (char ch in input)
+                stack.Push(ch);
+
+            var reversed = new StringBuilder();
+            while (stack.Count != 0)
+                reversed.Append(stack.Pop());
+
+            return reversed.ToString();
+        }
+
+    } // StringReverser class
+
+} // Stacks namespace
