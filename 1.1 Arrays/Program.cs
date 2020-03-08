ea39595896e232b522d4774da627720aa6e61f91
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-08 15:00:48 -0700

    Implement created Array class

--- 1.1 Arrays/Program.cs
+++ 1.1 Arrays/Program.cs
@@ -16,53 +16,53 @@ namespace Arrays
             Array numbers = new Array(3);
 
             // insert method
-            numbers.insert(10);
-            numbers.insert(20);
-            numbers.insert(30);
-            numbers.insert(40);
+            numbers.Insert(10);
+            numbers.Insert(20);
+            numbers.Insert(30);
+            numbers.Insert(40);
 
             // removeAt method
-            numbers.removeAt(3);
+            numbers.RemoveAt(3);
 
             // indexOf method
-            Console.WriteLine(numbers.indexOf(10));
+            Console.WriteLine(numbers.IndexOf(10));
 
             // print method
-            numbers.print();
+            numbers.Print();
 
             // printArray method
-            numbers.printArray();
+            numbers.PrintArray();
 
 
             // ---------------------------------------- Exercises ----------------------------------------
 
             // max method
-            Console.WriteLine(numbers.max());
+            Console.WriteLine(numbers.Max());
 
             // intersect method
             // Create a new array to compare
             Array newNums = new Array(5);
-            newNums.insert(10);
-            newNums.insert(20);
-            newNums.insert(40);
-            newNums.insert(50);
-            newNums.insert(60);
+            newNums.Insert(10);
+            newNums.Insert(20);
+            newNums.Insert(40);
+            newNums.Insert(50);
+            newNums.Insert(60);
             Console.WriteLine("\nnewNums");
-            newNums.printArray();
+            newNums.PrintArray();
 
-            numbers.intersect(newNums).printArray();
+            numbers.Intersect(newNums).PrintArray();
 
             // reverse method
-            numbers.reverse();
-            numbers.printArray();
+            numbers.Reverse();
+            numbers.PrintArray();
 
             Array nullArr = new Array(0);
-            nullArr.reverse();
-            nullArr.printArray();
+            nullArr.Reverse();
+            nullArr.PrintArray();
 
             // insertAt method
-            numbers.insertAt(15, 2);
-            numbers.printArray();
+            numbers.InsertAt(15, 2);
+            numbers.PrintArray();
 
         } // Main method
 
