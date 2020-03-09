374ee43d6206b2f0158ea100f1eab621eb00de04
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-09 11:49:42 -0700

    Bucket Sort

---
+++ 3.1 SortingAlgorithms/BucketSort.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class BucketSort
+    {
+        public void Sort(int[] array, int numberOfBuckets)
+        {
+            var i = 0;
+            foreach (var bucket in CreateBuckets(array, numberOfBuckets))
+            {
+                bucket.Sort();
+                foreach (var item in bucket)
+                    array[i++] = item;
+            }
+        }
+
+        private List<List<int>> CreateBuckets(int[] array, int numberOfBuckets)
+        {
+             var buckets = new List<List<int>>();
+            for (int i = 0; i < numberOfBuckets; i++)
+                buckets.Add(new List<int>());
+
+            foreach (int item in array)
+                buckets[item / numberOfBuckets].Add(item);
+
+            return buckets;
+        }
+    }
+}
