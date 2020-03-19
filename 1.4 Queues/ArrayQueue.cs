b9d8ec89ce0ac971aae9f2c20c9efea103942567
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-18 21:32:19 -0700

    Create a Queue class with an Array

---
+++ 1.4 Queues/ArrayQueue.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class ArrayQueue
+    {
+        private int[] items;
+        private int rear;
+        private int front;
+        private int count;
+
+        public ArrayQueue(int capacity)
+        {
+            items = new int[capacity];
+        }
+
+        public void Enqueue(int item)
+        {
+            if (isFull())
+                throw new InvalidOperationException();
+
+            items[rear] = item;
+            rear = (rear + 1) % items.Length;
+            count++;
+        }
+
+        public int Dequeue()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException();
+
+            var item = items[front];
+            items[front] = 0;
+            front = (front + 1) % items.Length;
+            count--;
+            return item;
+        }
+
+        public int Peek()
+        {
+            if (isEmpty())
+                throw new InvalidOperationException();
+
+            return items[front];
+        }
+
+        public bool isEmpty()
+        {
+            return count == 0;
+        }
+
+        public bool isFull()
+        {
+            return count == items.Length;
+        }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", items)}]";
+        }
+
+    } // ArrayQueue class
+
+} // Queue namespace
