c9b6375a7149627294b1cc735eb1134b43b2e98f
Da Hye (Emily) Park <da.h.park@outlook.com>
2020-03-18 21:32:55 -0700

    Stack with Two Queues Exercise

---
+++ 1.4 Queues/StackWithTwoQueues.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    class StackWithTwoQueues
+    {
+        // ------------------------------ Exercise ------------------------------
+        // 2 - Build a stack using two queues.
+        //     Implement the following operations and calculate their runtime complexities.
+        //      - push
+        //      - pop
+        //      - peek
+        //      - size
+        //      - isEmpty
+
+        private Queue<int> queue1 = new Queue<int>();
+        private Queue<int> queue2 = new Queue<int>();
+        private int top;
+
+        public void push(int item)
+        {
+            queue1.Enqueue(item);
+            top = item;
+        }
+
+        public int pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            while (queue1.Count > 1)
+            {
+                top = queue1.Dequeue();
+                queue2.Enqueue(top);
+            }
+
+            SwapQueues();
+            return queue2.Dequeue();
+        }
+
+        public int peek()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException();
+
+            return top;
+        }
+
+        public int size()
+        {
+            return queue1.Count;
+        }
+
+        public void SwapQueues()
+        {
+            (queue1, queue2) = (queue2, queue1);
+        }
+
+        public bool IsEmpty()
+        {
+            return queue1.Count == 0;
+        }
+
+        public override string ToString()
+        {
+            return $"[{string.Join(", ", queue1)}]";
+        }
+
+    } // StackWithTwoQueues class
+
+} // Queues namespace
